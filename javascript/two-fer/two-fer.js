8ea819b095b6ab6d1f8eca5a6239e865aa6015dc
michelle levine <michelle.levine@nike.com>
2018-02-26 11:19:48 -0800

    2. two-fer 2.0 --> swapped if statement for ternary operator

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -1,16 +1,8 @@
 var TwoFer = function () { };
 
 TwoFer.prototype.twoFer = function (who) {
-  // your code goes here
-  // You will have to use the parameter who
-  // in some way. In this example, it is just
-  // returned, but your solution will have to
-  // use a conditional.
-  if (!who) {
-    who = 'you';
-  }
 
-  let message = `One for ${who}, one for me.`;
+  let message = `One for ${who ? who : 'you'}, one for me.`;
 
   return message;
 };
